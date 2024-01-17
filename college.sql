ee11be70edf389e4c7a9fbda48a21f9b491b0d22
Manish J R <101006518+manishjr26@users.noreply.github.com>
2024-01-17 18:09:34 +0530

    Add files via upload

---
+++ college.sql
@@ -0,0 +1,15 @@
+1028	Adarsh	Adarsh@gmail.com
+1029	Bhanu	Bhanu@gmail.com
+1030	Mohith	Mohith@gmail.com
+1037	Bhuvan	bhuvan@ghmail.com
+1039	Ravindra	ravindra@gmail.com
+1040	Jayanth	jay@gmail.com
+1043	Kishore	ksihore@gmail.com
+1044	Tharun	tharun@gmail.com
+1045	Kiran	kiran@gmail.com
+1046	Vijay	Vijay@gmail.com
+1047	Thaeer	thaeer@gmail.com
+1048	Rohith	rohith@gmail.com
+1061	Vamshi	vamshi@gmail.com
+1062	Chenasvau	cheu@gmail.com
+		
